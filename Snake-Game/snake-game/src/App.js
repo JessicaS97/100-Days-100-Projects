6edf0618a76e5910bfd4d5e59d179b5e5451cc3e
Jessica J Sun <jessi.slm@hotmail.com>
2020-02-08 21:31:26 +1100

    Random food

--- Snake-Game/snake-game/src/App.js
+++ Snake-Game/snake-game/src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import Snake from './Snake'
 import Food from './Food'
 
+const getRandom = () => {
+  let min = 1
+  let max = 98
+  let x = Math.floor((Math.random() * (max-min+1)+min)/2)*2
+  let y = Math.floor((Math.random() * (max-min+1)+min)/2)*2
+  return [x,y]
+}
+
 class App extends Component {
 
   state = {
-    food: [6,8],
+    food: getRandom(),
     snakeBody: [
       [0,0],
       [3, 0]
