f8b4572f1e9fbc2c5bed3f2d33c2dab91db8b1e8
Jessica J Sun <jessi.slm@hotmail.com>
2020-02-08 21:57:22 +1100

    commits

--- Snake-Game/snake-game/src/App.js
+++ Snake-Game/snake-game/src/App.js
@@ -14,11 +14,65 @@ const getRandom = () => {
 class App extends Component {
 
   state = {
+    direction: 'right',
+    speed: 200,
     food: getRandom(),
     snakeBody: [
       [0,0],
       [3, 0]
     ]
+  }
+
+  componentDidMount() {
+    setInterval(this.moveSnake, this.state.speed)
+    document.onkeydown = this.handleKey
+  }
+
+  handleKey = e => {
+    e = e || window.event
+    switch(e.keyCode) {
+      case 38:
+        this.setState({direction: 'up'})
+        break;
+      case 40:
+        this.setState({direction:'down'})
+        break;
+      case 37: 
+        this.setState({direction:'left'})
+        break;
+      case 39:
+        this.setState({direction:'right'})
+        break;
+      default:
+        break;
+    }
+  }
+
+  moveSnake = () => {
+    let body = [...this.state.snakeBody]
+    let head = body[body.length - 1]
+
+    switch(this.state.direction) {
+      case 'right':
+        head = [head[0] + 3, head[1]]
+        break
+      case 'left':
+        head = [head[0] - 3, head[1]]
+        break
+      case 'down':
+        head = [head[0], head[1] + 3]
+        break
+      case 'up':
+        head = [head[0], head[1] - 3]
+        break
+      default:
+        break
+    }
+    body.push(head)
+    body.shift()
+    this.setState({
+      snakeBody: body
+    })
   }
 
   render() {
