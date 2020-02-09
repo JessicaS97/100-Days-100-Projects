94713ac93892a0d542c85581d15b13bc51e1688d
Jessica J Sun <jessi.slm@hotmail.com>
2020-02-09 11:25:01 +1100

    Updates

---
+++ precache-manifest.5e8b37481a3cedf5fd50fe4973b52845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833f6f53ed728f10f1497fa1a28142de",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "0d273ea275eee33bdcb6",
+    "url": "/myapp/static/css/main.ffeb8f20.chunk.css"
+  },
+  {
+    "revision": "b49a5bd460226bce9ed4",
+    "url": "/myapp/static/js/2.42c198cf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myapp/static/js/2.42c198cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d273ea275eee33bdcb6",
+    "url": "/myapp/static/js/main.7c50e864.chunk.js"
+  },
+  {
+    "revision": "ef72da296c90cdf40540",
+    "url": "/myapp/static/js/runtime-main.5b1a8f59.js"
+  }
+]);
